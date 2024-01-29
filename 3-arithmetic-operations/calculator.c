15a5ed2706751c2d4b0ac09a49cd494d5db74e3a
Sarmad426 <sarmadrafique040@gmail.com>
2024-01-29 09:13:32 +0500

    Created Calculator program in c

---
+++ 3-arithmetic-operations/calculator.c
@@ -0,0 +1,9 @@
+#include<string.h>
+
+int main(){
+	int num1 = 10, num2 = 5;
+	printf("Addition = %i\n",num1+num2);
+	printf("Subtraction = %i\n",num1-num2);
+	printf("Multiplication = %i\n",num1*num2);
+	printf("Division = %i\n",num1/num2);
+}
