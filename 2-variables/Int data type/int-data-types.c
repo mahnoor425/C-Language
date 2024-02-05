d3b5ab0677646f2f580ca7928d7357b1d6c406c5
mahnoor425 <mahnoorrafique04086@gmail.com>
2024-02-05 14:00:40 +0500

    Renamed the file and added int data types code

---
+++ 2-variables/Int data type/int-data-types.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int num1 = 5;
+    short int num2 = 10;
+    unsigned int num3= 15;
+    long int num4= 20;
+    unsigned long int num5 = 25;
+    
+    printf("%i\n",num1);
+    printf("%i\n",num2);
+    printf("%i\n",num3);
+    printf("%i\n",num4);
+    printf("%i\n",num5);
+}
--- 2-variables/Int data type/int-variable.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-	// Integer Variable
-	unsigned long int num = 11;
-	printf("%i",num);
-}
-
